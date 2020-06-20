09c5928c0817363e4b4cd5efc964c70b736e4271
Sal7_one <oo32dash1@gmail.com>
2020-06-20 11:06:38 +0300

    Added icons

--- script.js
+++ script.js
@@ -4,43 +4,42 @@ let root = document.documentElement;
 let savedprefrence = ""
 
 
-function Set(thingy){
-  chrome.storage.local.set({mykey: thingy});
+function Set(thingy) {
+  chrome.storage.local.set({ mykey: thingy });
 }
 
-window.onload = function(){
+window.onload = function () {
 
   async function Get() {
     return new Promise((resolve, reject) => {
-        try {
-            chrome.storage.local.get(mykey, function (value) {
-                resolve(value.mykey);
-            })
-        }
-        catch (ex) {
-            reject(ex);
-        }
+      try {
+        chrome.storage.local.get(mykey, function (value) {
+          resolve(value.mykey);
+        })
+      }
+      catch (ex) {
+        reject(ex);
+      }
     });
   }
-  
-  Get(mykey).then(result => {
-  // result is here
-    App(result)
-   });
 
-   root.addEventListener
+  Get(mykey).then(result => {
+    // result is here
+    App(result)
+  });
+
 }
 
 
-function App(result){
-   savedprefrence = result
-   if(savedprefrence == 'shown'){
-     showthem();
-   }else if(savedprefrence == 'hidden'){
-     hidethem();
-   }else{
-     hidethem();
-   }
+function App(result) {
+  savedprefrence = result
+  if (savedprefrence == 'shown') {
+    showthem();
+  } else if (savedprefrence == 'hidden') {
+    hidethem();
+  } else {
+    hidethem();
+  }
 
 }
 
@@ -59,17 +58,18 @@ function showthem() {
   swtch.setAttribute("unchecked", true)
 }
 
-swtch.onclick = function(){
-  if(swtch.checked){
-   hidethem();
-   refresh();
-  }else{
-   showthem();
-   refresh();
+swtch.onclick = function () {
+  if (swtch.checked) {
+    hidethem();
+    refresh();
+  } else {
+    showthem();
+    refresh();
   }
 }
 
-function refresh(){
-  chrome.tabs.getSelected(null, function(tab) {
+function refresh() {
+  chrome.tabs.getSelected(null, function (tab) {
     chrome.tabs.reload(tab.id);
-});}
+  });
+}
