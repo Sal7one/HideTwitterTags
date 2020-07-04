95e13db7552d323985866d0e48419de933a3f050
Sal7one <alanazi.saleh.77@gmail.com>
2020-07-04 23:29:19 +0300

    search appears and it works wtihout and account and fixed edit profile not working

--- script.js
+++ script.js
@@ -1,61 +1,66 @@
-var mykey = "mykey"
-let swtch = document.getElementById("switchbtn")
+var mykey = "mykey";
+var mycookie = 0;
+let swtch = document.getElementById("switchbtn");
 let root = document.documentElement;
-let savedprefrence = ""
+let savedprefrence = "";
 
-
+// Who to folow .r-1bro5k0,
 function Set(thingy) {
   chrome.storage.local.set({ mykey: thingy });
 }
 
 window.onload = function () {
+  mycookie = document.cookie.length;
 
   async function Get() {
     return new Promise((resolve, reject) => {
       try {
         chrome.storage.local.get(mykey, function (value) {
           resolve(value.mykey);
-        })
-      }
-      catch (ex) {
+        });
+      } catch (ex) {
         reject(ex);
       }
     });
   }
 
-  Get(mykey).then(result => {
+  Get(mykey).then((result) => {
     // result is here
-    App(result)
+    App(result);
   });
-
-}
-
+};
 
 function App(result) {
-  savedprefrence = result
-  if (savedprefrence == 'shown') {
+  savedprefrence = result;
+  if (savedprefrence == "shown") {
     showthem();
-  } else if (savedprefrence == 'hidden') {
+  } else if (savedprefrence == "hidden") {
     hidethem();
   } else {
     hidethem();
   }
-
 }
-
 
 function hidethem() {
-  root.style.setProperty('--status', 'none')
+  if (mycookie > 300) {
+    root.style.setProperty("--list1", "inline");
+    root.style.setProperty("--list0", "none");
+  } else {
+    root.style.setProperty("--list1", "none");
+    root.style.setProperty("--list0", "inline");
+  }
+  root.style.setProperty("--status", "none");
 
-  Set("hidden")
-  swtch.setAttribute("checked", true)
-
+  Set("hidden");
+  swtch.setAttribute("checked", true);
 }
 function showthem() {
-  root.style.setProperty('--status', 'inline')
+  root.style.setProperty("--list1", "inline");
+  root.style.setProperty("--list0", "inline");
+  root.style.setProperty("--status", "inline");
 
-  Set("shown")
-  swtch.setAttribute("unchecked", true)
+  Set("shown");
+  swtch.setAttribute("unchecked", true);
 }
 
 swtch.onclick = function () {
@@ -66,7 +71,7 @@ swtch.onclick = function () {
     showthem();
     refresh();
   }
-}
+};
 
 function refresh() {
   chrome.tabs.getSelected(null, function (tab) {
