77c948c1ccd872577b132c50c01610e5ccb1ae53
Sal7_one <oo32dash1@gmail.com>
2020-07-06 14:58:47 +0300

    now it works with profile pages and non logged in users

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 var mykey = "mykey";
+var loggedin = true;
 let swtch = document.getElementById("switchbtn");
 let root = document.documentElement;
-let loggedin = true;
+let home = "https://twitter.com/home";
 let savedprefrence = "";
 let cookieValue = 0;
+
+cookieValue = document.cookie.split("; ").find((row) => row.startsWith("twid"));
+
+if (cookieValue == undefined) loggedin = false;
 
 // Who to folow .r-1bro5k0,
 function Set(thingy) {
@@ -11,12 +16,6 @@ function Set(thingy) {
 }
 
 window.onload = function () {
-  cookieValue = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith("twid"));
-
-  if (cookieValue == undefined) loggedin = false;
-
   async function Get() {
     return new Promise((resolve, reject) => {
       try {
@@ -47,7 +46,14 @@ function App(result) {
 }
 
 function hidethem() {
-  root.style.setProperty("--list1", "inline");
+  if (window.loggedin) {
+    root.style.setProperty("--list1", "inline");
+    if (window.location == home) root.style.setProperty("--list0", "none");
+    else root.style.setProperty("--list0", "inline");
+  } else {
+    root.style.setProperty("--list1", "none");
+    root.style.setProperty("--list0", "inline");
+  }
   root.style.setProperty("--status", "none");
 
   Set("hidden");
