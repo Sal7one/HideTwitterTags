2a61e335e8b113474d8ed52e5ca83337a108ca90
Sal7_one <oo32dash1@gmail.com>
2020-07-17 13:37:06 +0300

    nope

--- script.js
+++ script.js
@@ -1,79 +1,144 @@
-var mykey = "mykey";
-var loggedin = true;
-let swtch = document.getElementById("switchbtn");
 let root = document.documentElement;
 let home = "https://twitter.com/home";
+let tagbtn = document.getElementById("switchbtntag");
+let whobtn = document.getElementById("switchbtnwho");
+let footbtn = document.getElementById("switchbtnfoot");
+let relvbtn = document.getElementById("switchbtnrelv");
+var tags = "tags";
+var whotofollow = "whotofollow";
+var relventppl = "relventppl";
+var footer = "footer";
 let savedprefrence = "";
-let cookieValue = 0;
+let results = [];
 
-cookieValue = document.cookie.split("; ").find((row) => row.startsWith("twid"));
-
-if (cookieValue == undefined) loggedin = false;
-
-// Who to folow .r-1bro5k0,
-function Set(thingy) {
-  chrome.storage.local.set({ mykey: thingy });
+function Set(keys, thingy) {
+  alert("setting " , keys, "  s" , thing)
+  var obj = {};
+  var key = keys;
+  obj[key] += thingy;
+  chrome.storage.local.set(obj);
 }
 
 window.onload = function () {
-  async function Get() {
+  async function Get(x) {
     return new Promise((resolve, reject) => {
       try {
-        chrome.storage.local.get(mykey, function (value) {
-          resolve(value.mykey);
+        chrome.storage.local.get(tags, function (value) {
+          if (value.tags != "shown " || value.tags != "hidden") {
+            results[0] = value.tags;
+            alert("getting  ", value.tags);
+          } else alert("ss");
         });
+        // chrome.storage.local.get(whotofollow, function (value) {
+        //   results[1] = await value.whotofollow;
+        // });
+        // chrome.storage.local.get(relventppl, function (value) {
+        //   results[2] = await value.relventppl;
+        // });
+        // chrome.storage.local.get(footer, function (value) {
+        //   results[3] = await value.footer;
+        // });
+
+        resolve(results);
       } catch (ex) {
         reject(ex);
       }
     });
   }
 
-  Get(mykey).then((result) => {
-    // result is here
-    App(result);
+  Get(something).then((result) => {
+    alert("my result is " + result);
   });
 };
 
 function App(result) {
-  savedprefrence = result;
-  if (savedprefrence == "shown") {
-    showthem();
-  } else if (savedprefrence == "hidden") {
-    hidethem();
-  } else {
-    hidethem();
+  for (i = 0; i < result.length; i++) {
+    if (result[i] == "shown") {
+      if (i == 0) {
+        showthem(tags);
+        tagbtn.setAttribute("checked", true);
+      }
+      if (i == 1) {
+        showthem(whotofollow);
+        whobtn.setAttribute("checked", true);
+      }
+      if (i == 2) {
+        showthem(relventppl);
+        relvbtn.setAttribute("checked", true);
+      }
+      if (i == 3) {
+        showthem(footer);
+        footbtn.setAttribute("checked", true);
+      }
+    } else {
+      if (i == 1) {
+        hidethem(tags);
+        tagbtn.setAttribute("unchecked", true);
+      }
+      if (i == 2) {
+        hidethem(whotofollow);
+        whobtn.setAttribute("unchecked", true);
+      }
+      if (i == 3) {
+        hidethem(relventppl);
+        relvbtn.setAttribute("unchecked", true);
+      }
+      if (i == 4) {
+        hidethem(footer);
+        footbtn.setAttribute("unchecked", true);
+      }
+    }
   }
 }
 
-function hidethem() {
-  if (window.loggedin) {
-    root.style.setProperty("--list1", "inline");
-    if (window.location == home) root.style.setProperty("--list0", "none");
-    else root.style.setProperty("--list0", "inline");
-  } else {
-    root.style.setProperty("--list1", "none");
-    root.style.setProperty("--list0", "inline");
-  }
-  root.style.setProperty("--status", "none");
-
-  Set("hidden");
-  swtch.setAttribute("checked", true);
+function hidethem(key) {
+  alert("my key is " + key);
+  root.style.setProperty("--" + key, "none");
+  Set(key, "hidden");
 }
-function showthem() {
-  root.style.setProperty("--list1", "inline");
-  root.style.setProperty("--list0", "inline");
-  root.style.setProperty("--status", "inline");
-
-  Set("shown");
-  swtch.setAttribute("unchecked", true);
+function showthem(key) {
+  root.style.setProperty("--" + key, "inline");
+  Set(key, "shown");
 }
 
-swtch.onclick = function () {
-  if (swtch.checked) {
-    hidethem();
+//Todo make it on click function and pass values
+
+tagbtn.onclick = function () {
+  if (tagbtn.checked) {
+    hidethem(tags);
     refresh();
   } else {
-    showthem();
+    showthem(tags);
+    refresh();
+  }
+};
+
+whobtn.onclick = function () {
+  if (whobtn.checked) {
+    hidethem(whotofollow);
+    refresh();
+  } else {
+    showthem(whotofollow);
+    refresh();
+  }
+};
+
+relvbtn.onclick = function () {
+  if (relvbtn.checked) {
+    hidethem(relventppl);
+    refresh();
+  } else {
+    showthem(relventppl);
+    refresh();
+  }
+};
+
+footbtn.onclick = function () {
+  if (footbtn.checked) {
+    hidethem(footer);
+    refresh();
+  } else {
+    showthem(footer);
     refresh();
   }
 };
