485649159920f64f0318f597c35b04f24d1733b4
Sal7one <alanazi.saleh.77@gmail.com>
2020-09-20 06:48:18 +0300

    refactoring and trying new system

--- script.js
+++ script.js
@@ -1,84 +1,79 @@
 var loggedin = true;
-let swtch = document.getElementById("switchbtn");
+let mybtns = document.querySelectorAll("[data-num]");
 let root = document.documentElement;
 let home = "https://twitter.com/home";
-var allofthem = "allofthem";
 var tags = "tags";
 var whotofollow = "whotofollow";
 var relventppl = "relventppl";
 var footer = "footer";
-var signup = "signup";
 let savedprefrence = "";
 
 // Who to folow .r-1bro5k0,
 function Set(key, thingy) {
-  chrome.storage.local.set({ [key]: thingy });
+  chrome.storage.local.set({ [key]: [thingy] });
 }
 
 window.onload = function () {
-  async function Get() {
-    return new Promise((resolve, reject) => {
-      try {
-        let results = [];
-        chrome.storage.local.get(allofthem, function (value) {
-          results[0] = value.allofthem;
-        });
-        chrome.storage.local.get(tags, function (value) {
-          results[1] = value.tags;
-        });
-        chrome.storage.local.get(whotofollow, function (value) {
-          results[2] = value.whotofollow;
-        });
-        chrome.storage.local.get(relventppl, function (value) {
-          results[3] = value.relventppl;
-        });
-        chrome.storage.local.get(footer, function (value) {
-          results[4] = value.footer;
-        });
-        resolve(results);
-      } catch (ex) {
-        reject(ex);
-      }
+  var savedvalues = [];
+
+  function printme(i, values) {
+    savedvalues[i] = values;
+
+    if (savedvalues.length == 4) App(savedvalues);
+  }
+  function Get(num, key, mycall) {
+    chrome.storage.local.get(key, function (items) {
+      return mycall(num, items[key]);
     });
   }
-
-  Get(allofthem).then((result) => {
-    // result is here
-    App(result);
-  });
+  Get(0, tags, printme);
+  Get(1, whotofollow, printme);
+  Get(2, relventppl, printme);
+  Get(3, footer, printme);
 };
 
 function App(result) {
   savedprefrence = result;
-  if (savedprefrence == "shown") {
-    showthem();
-  } else if (savedprefrence == "hidden") {
-    hidethem();
-  } else {
-    hidethem();
+  NumofSavedValues = savedprefrence.length;
+
+  for (i = 0; i < NumofSavedValues; i++) {
+    if (savedprefrence[i] == "shown") {
+      root.style.setProperty(`--none`, savedprefrence[i]);
+    } else {
+      root.style.setProperty(`--none`, savedprefrence[i]);
+    }
   }
 }
 
-function hidethem(key) {
-  Set(key, "hidden");
-  swtch.setAttribute("checked", true);
-}
-function showthem(key) {
-  //  root.style.setProperty("--status", "inline");
-
-  Set(key, "shown");
-  swtch.setAttribute("unchecked", true);
-}
-
-swtch.onclick = function () {
-  if (swtch.checked) {
-    hidethem();
-    refresh();
-  } else {
-    showthem();
-    refresh();
+function changestatus(element, status) {
+  let key = element.getAttribute("id");
+  let styletype = "none";
+  let savedvalue = "hidden";
+  let attribute = "checked";
+  if (status == "show") {
+    styletype = "inline";
+    attribute = "unchecked";
+    savedvalue = "shown";
   }
-};
+  try {
+    element.setAttribute(attribute, true);
+    root.style.setProperty(`--${key}`, styletype);
+    Set(key, savedvalue);
+    refresh();
+  } catch (error) {}
+}
+
+mybtns.forEach((element) => {
+  element.addEventListener("click", function () {
+    if (element.checked) {
+      changestatus(element, "hide");
+      refresh();
+    } else {
+      changestatus(element, "show");
+      refresh();
+    }
+  });
+});
 
 function refresh() {
   chrome.tabs.getSelected(null, function (tab) {
