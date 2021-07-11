09840b1bc22631314002f6b5edf1477dff3c19ff
Sal7one <Alanazi.saleh.77@gmail.com>
2021-07-11 20:58:43 +0300

    added topic support

--- script.js
+++ script.js
@@ -10,17 +10,25 @@ var topics = "topics";
 var savedvalues = "";
 let keey = "";
 
-window.onload = function () {
-  Get().then((value) => {
-    App(value);
-  });
-};
+let gettingItem = browser.storage.local.get();
+gettingItem.then((gotvalues) => {
+  values = [];
+
+  values.push(gotvalues.search);
+  values.push(gotvalues.tags);
+  values.push(gotvalues.whotofollow);
+  values.push(gotvalues.relventppl);
+  values.push(gotvalues.footer);
+  values.push(gotvalues.explore);
+  values.push(gotvalues.topics);
+
+  App(values);
+});
 
 function App(result) {
   savedvalues = result;
   keey = [search,tags, whotofollow, relventppl, footer, explore, topics];
   NumofSavedValues = savedvalues.length;
-
 
   if (NumofSavedValues > 0) {
     for (i = 0; i < NumofSavedValues; i++) {
@@ -43,10 +51,8 @@ function App(result) {
       try {
         mybtns[i].setAttribute("checked", true);
       } catch (error) {}
-      if(keey[i] == "search" || keey[i] == "explore"){
-        console.log("found")
-        continue;
-      }
+      if(keey[i] == "search" || keey[i] == explore)
+      continue;
       changepagestatus(keey[i], "hidden");
       Set(keey[i], "hidden");
     }
@@ -60,7 +66,7 @@ function changepagestatus(key, status) {
     else
     root.style.setProperty(`--${key}`, "inline");
   } else {
-   root.style.setProperty(`--${key}`, "none");
+    root.style.setProperty(`--${key}`, "none");
     FailSafeChecker(key)
   }
 }
@@ -79,41 +85,25 @@ mybtns.forEach((element) => {
 });
 
 function refresh() {
-  chrome.tabs.getSelected(null, function (tab) {
-    chrome.tabs.reload(tab.id);
-  });
+  browser.tabs.reload();
 }
 
-function Get() {
-  return new Promise(function (resolve, _reject) {
-    chrome.storage.local.get(null, function (items) {
-      let values = [];
-      values[0] = items[search];
-      values[1] = items[tags];
-      values[2] = items[whotofollow];
-      values[3] = items[relventppl];
-      values[4] = items[footer];
-      values[5] = items[explore];
-      values[6] = items[topics];
-      resolve(values);
-    });
-  });
-}
 function Set(key, thingy) {
-  chrome.storage.local.set({ [key]: [thingy] });
+  browser.storage.local.set({ [key]: thingy });
 }
+
 
 // Backup plan if Twitter change their elemnts
-
 function FailSafeChecker(key){
   
   // Supported languages are English, Arabic, German, Spanish
   searchlangs = [ `[aria-label="Search Twitter"]`,`[aria-label="البحث في تويتر"]`,`[aria-label="Twitter durchsuchen"]`,`[aria-label="Buscar en Twitter"]` ]
   taglangs = [`[aria-label="Timeline: Trending now"]`,  `[aria-label="الخطّ الزمنيّ: المتداوَل الآن"]`,`[aria-label="Timeline: Aktuelle Trends"]`,`[aria-label="Cronología: Tendencias del momento"]` ]
   whotofollowlangs = [`[aria-label="Who to follow"]` , `[aria-label="اقتراحات المتابعة"]`,`[aria-label="Wem folgen?"]`,`[aria-label="A quién seguir"]` ]
-  relventppllangs = [`[aria-label="Relevant people"]`, `[aria-label="الأشخاص ذوو الصلة"]`,`[aria-label="Relevante Personen"]`,`[aria-label="Personas relevantes"]`, `[aria-label="प्रासंगिक लोग"]`,`[aria-label="Подходящие люди"]`, `[aria-label="Пов’язані люди"]`,` [aria-label="Persone pertinenti"]`, `[aria-label="関連性の高いアカウント"]`]
+  relventppllangs = [`[aria-label="Relevant people"]`, `[aria-label="الأشخاص ذوو الصلة"]`,`[aria-label="Relevante Personen"]`,`[aria-label="Personas relevantes"]`]
   footerlangs = [`[aria-label="Footer"]`, `[aria-label="الشريط السُفلي"] `,`[aria-label="Fußzeile"]`,`[aria-label="Pie de página"]`]
   explorelangs = [`a[href="/explore"]`]
+
   element = [0]
   
   // Selected elemnt
@@ -125,27 +115,13 @@ function FailSafeChecker(key){
     case relventppl: element =  relventppllangs
     break;
     case  footer: element = footerlangs
-    break; case search: element =  searchlangs
-    break; case explore: element =  explorelangs
+    break; 
+    case search: element =  searchlangs
     break;
-  default: return
+    case explore: element =  explorelangs
+    break;
+  default:  return
   }
-
-  if(key === tags){
-    try {
-      autoborderhider();
-      setTimeout(()=>{
-        autoborderhider();
-       },950)
- 
-
-    } catch (error) {
-      console.log("error in backup tags border")
-      console.log(error)
-    }
-    
-  }
-
   if(key === explore){
     explore =  document.querySelector(`a[href="/explore"]`)
     if(explore == null)
@@ -153,7 +129,6 @@ function FailSafeChecker(key){
     else
     explore.style.display = "none"
   }else{
-
   for(j = 0; j < element.length; j++){
   //Is the element here
   selectedElement = document.querySelector(element[j])
@@ -189,23 +164,4 @@ function findsidebar(element, BeforeSideBar){
     return findsidebar(element, BeforeSideBar)
   }
   return BeforeSideBar;
-}
-
-
-function autoborderhider(){
-  tagselemntwhite =  document.querySelector(".css-1dbjc4n.r-1ihkh82.r-1in3vh1.r-1867qdf.r-1phboty.r-rs99b7.r-1ifxtd0.r-1udh08x")
-  tagselemntDark =  document.querySelector(".css-1dbjc4n.r-1uaug3w.r-1uhd6vh.r-1867qdf.r-1phboty.r-rs99b7.r-1ifxtd0.r-1udh08x")
-  tagselemntblack =  document.querySelector(".css-1dbjc4n.r-1ysxnx4.r-k0dy70.r-1867qdf.r-1phboty.r-rs99b7.r-1ifxtd0.r-1udh08x")
- if(tagselemntwhite){
-  tagselemntwhite.style.setProperty("border-width","0px","important")
-  tagselemntwhite.style.setProperty("border-color","transparent","important")
- }
- if(tagselemntDark){
-  tagselemntDark.style.setProperty("border-width","0px","important")
-  tagselemntDark.style.setProperty("border-color","transparent","important")
- }
- if(tagselemntblack){
-  tagselemntblack.style.setProperty("border-width","0px","important")
-  tagselemntblack.style.setProperty("border-color","transparent","important")
- }
 }
